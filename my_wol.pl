0a33c45627f502eaf7ea83173e73ccd0a3f001aa
Varun Verma <vv311@ic.ac.uk>
2013-03-06 13:23:01 +0000

    finished question 3 and 4 now. Now need to implement the strategies

--- my_wol.pl
+++ my_wol.pl
@@ -4,34 +4,35 @@
 
 
 
-test_strategy( N, P1Strategy, P2Strategy) :-
+test_strategy(N, P1Strategy, P2Strategy) :-
     write('----------------------------------------------'), write('\n'),
-    write('P1 strategy: '), write(P1Strategy), write('\nP2 strategy: '), write(P2Strategy),write('\n'),
     statistics(runtime, [T0|_]),
-    test_strategy(N, P1Strategy, P2Strategy, Results, Games),
+    test_strategy_helper(N, P1Strategy, P2Strategy, Results, GamesMovesNumber),
     statistics(runtime, [T1|_]),
     T is T1 - T0,
     AvgT is T/N,
-    delete(Games,250,PGames),
+    delete(GamesMovesNumber,250,PGames),
     max_member(Max,PGames),
-    min_member(Min,Games),
-    sumlist(Games,SumGames), length(Games,GamesLength),
+    min_member(Min,GamesMovesNumber),
+    sumlist(GamesMovesNumber,SumGames), length(GamesMovesNumber,GamesLength),
     AvgGames is SumGames/GamesLength,
     findall(B, nth1(I,Results,r), Red), length(Red,RedWins),
     findall(B, nth1(I,Results,b), Blue), length(Blue,BlueWins),
     findall(B, nth1(I,Results,draw), Draws), length(Draws,DrawWins),
+    write('Number of draws:'), write(DrawWins), write('\n'), 
     write('Number of wins for player 1 (blue):'), write(BlueWins), write('\n'),
     write('Number of wins for player 2 (red):'), write(RedWins), write('\n'),
-    write('Number of draws:'), write(DrawWins), write('\n'),  
     write('Longest (non-exhaustive) game: '), write(Max), write('\n'),
     write('Shortest Game: '), write(Min), write('\n'),
-    write('Average Game Length (including exhaustives): '), write(AvgGames), write('\n'),
+    write('Average game length (including exhaustives): '), write(AvgGames), write('\n'),
     format('Average game time: ~3f sec.~n', [AvgT/1000]).
+    
+    
 
-test_strategy( 0, _, _, [], []) :- !.
-test_strategy( NGames, P1s, P2s, Results, Games) :-
+test_strategy_helper( 0, _, _, [], []) :- !.
+test_strategy_helper( NGames, P1s, P2s, Results, Games) :-
     play(quiet,P1s,P2s,NumMoves,Winner),
     N is NGames - 1,
-    test_strategy(N,P1s,P2s,R,G),
+    test_strategy_helper(N,P1s,P2s,R,G),
     append(R,[Winner],Results),
     append(G,[NumMoves],Games).
